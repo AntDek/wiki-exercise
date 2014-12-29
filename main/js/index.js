ec938c28212b6508a80e6a7019a17818d8399fad
Anton Dekterov <anton.dekterov@socialbakers.com>
2014-12-29 15:15:39 +0100

    BIG FIX - absolute path to index.html

--- main/js/index.js
+++ main/js/index.js
@@ -4,6 +4,9 @@ var express = require("express");
 // rest server routes
 var rest = require("./rest");
 
+// require path module
+path = require('path');
+
 // create express app
 var app = express()
 
@@ -13,9 +16,9 @@ app.use(express.static(__dirname + "/../public"));
 // attach server routes
 rest(app);
 
-// catch every requets and redirect it to index.html
+// catch every requests and redirect it to index.html
 app.get("/*", function(req, res) {
-	res.sendFile(__dirname + "/../public/index.html");
+	res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // start server on port 3000
